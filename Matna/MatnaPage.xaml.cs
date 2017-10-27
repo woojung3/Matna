145b910ab4cfdaf8518b33e97d4b723d0b4e7c22
sostlsz <sostlsz@naver.com>
2017-10-27 09:35:13 +0900

    Initial Commit

---
+++ Matna/MatnaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Matna
+{
+    public partial class MatnaPage : ContentPage
+    {
+        public MatnaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
